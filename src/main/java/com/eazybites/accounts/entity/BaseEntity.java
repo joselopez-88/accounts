c9349869230f0bf5f3b17bd103937a0084d5888f
joselopez <alberto.esteva88@gmail.com>
2025-06-10 20:18:18 -0600

    Creacion de una superclase para entities que comparten los mismos campos, el dato se guardara en su tabla respectiva

---
+++ src/main/java/com/eazybites/accounts/entity/BaseEntity.java
@@ -0,0 +1,23 @@
+package com.eazybites.accounts.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+@MappedSuperclass// Esto indica que la clase BaseEntity será una superclase para otras entidades
+@Getter
+@Setter
+@ToString
+public class BaseEntity {
+    @Column(updatable = false)//Indica que el dato no se guarda en un update, solo en el insert
+    private LocalDateTime createdAt;
+    @Column(updatable = false)
+    private LocalDateTime createdBy;
+    @Column(insertable = false)//Indica que el dato no se guarda en un insert sino en un update
+    private LocalDateTime updatedAt;
+    @Column(insertable = false)
+    private LocalDateTime updatedBy;
+}
