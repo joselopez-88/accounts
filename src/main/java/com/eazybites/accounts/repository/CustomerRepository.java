5a83ba4c88a4746514727d75eca9e243b17b6554
joselopez <alberto.esteva88@gmail.com>
2025-06-11 22:37:02 -0600

    Agregando metodos al CustomerRepository

--- src/main/java/com/eazybites/accounts/repository/CustomerRepository.java
+++ src/main/java/com/eazybites/accounts/repository/CustomerRepository.java
@@ -1,10 +1,13 @@
 package com.eazybites.accounts.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.eazybites.accounts.entity.Customer;
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long>{
-
+    Optional<Customer> findByMobileNumber(String mobileNumber);
+    boolean existsByMobileNumber(String mobileNumber);
 }
