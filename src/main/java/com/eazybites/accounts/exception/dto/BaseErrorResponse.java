421882ad03a9b37e579fb6753977ba9c4ae7b08d
joselopez <alberto.esteva88@gmail.com>
2025-06-11 22:35:38 -0600

    Cambiando de paquete los dto de errores

---
+++ src/main/java/com/eazybites/accounts/exception/dto/BaseErrorResponse.java
@@ -0,0 +1,18 @@
+package com.eazybites.accounts.exception.dto;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public abstract class BaseErrorResponse implements Serializable {
+    private String apiPath;
+    private String status;
+    private Integer code;
+    private LocalDateTime errorTime;
+}
---
+++ src/main/java/com/eazybites/accounts/exception/dto/ErrorResponse.java
@@ -0,0 +1,14 @@
+package com.eazybites.accounts.exception.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse extends BaseErrorResponse {
+    private String message;
+}
