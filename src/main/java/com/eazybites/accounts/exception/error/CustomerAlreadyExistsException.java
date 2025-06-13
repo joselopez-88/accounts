1ceb70d27a5cea71419695e8ae5cd136c9eee371
joselopez <alberto.esteva88@gmail.com>
2025-06-12 20:34:12 -0600

    Eliminando anotacion de error innecesaria

--- src/main/java/com/eazybites/accounts/exception/error/CustomerAlreadyExistsException.java
+++ src/main/java/com/eazybites/accounts/exception/error/CustomerAlreadyExistsException.java
@@ -1,9 +1,5 @@
 package com.eazybites.accounts.exception.error;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class CustomerAlreadyExistsException extends RuntimeException {
     
     public CustomerAlreadyExistsException(String message) {
