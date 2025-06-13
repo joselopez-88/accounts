55d2e7c6b9aa7254ddfc4c61816ef79def128428
joselopez <alberto.esteva88@gmail.com>
2025-06-12 19:55:05 -0600

    Excluyendo campos con anotacion Mapping en cada mapper -.-

--- src/main/java/com/eazybites/accounts/mapper/AccountsMapper.java
+++ src/main/java/com/eazybites/accounts/mapper/AccountsMapper.java
@@ -1,6 +1,7 @@
 package com.eazybites.accounts.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import com.eazybites.accounts.model.dto.request.AccountsDto;
 import com.eazybites.accounts.model.entity.Accounts;
@@ -8,6 +9,11 @@ import com.eazybites.accounts.model.entity.Accounts;
 @Mapper(componentModel = "spring")
 public interface AccountsMapper {
     AccountsDto mapToAccountsDto(Accounts accounts);
+    @Mapping(target = "accountNumber", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "createdBy", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "updatedBy", ignore = true)
     Accounts mapToAccounts(AccountsDto accountsDto);
 
 }
--- src/main/java/com/eazybites/accounts/mapper/CustomerMapper.java
+++ src/main/java/com/eazybites/accounts/mapper/CustomerMapper.java
@@ -1,12 +1,18 @@
 package com.eazybites.accounts.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import com.eazybites.accounts.model.dto.request.CustomerDto;
 import com.eazybites.accounts.model.entity.Customer;
 
 @Mapper(componentModel = "spring")
-public interface CustomerMapper {
+public interface CustomerMapper  {
     CustomerDto mapToCustomerDto(Customer customer);
+    @Mapping(target = "customerId", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "createdBy", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "updatedBy", ignore = true)
     Customer mapToCustomer(CustomerDto customerDto);
 }
