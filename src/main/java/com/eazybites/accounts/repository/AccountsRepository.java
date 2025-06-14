a0989c95a55a8e4af6e869fc2b57fe3fb00c9654
joselopez <alberto.esteva88@gmail.com>
2025-06-13 23:47:37 -0600

    Agregando metodo de busqueda de Account por numero telefonico

--- src/main/java/com/eazybites/accounts/repository/AccountsRepository.java
+++ src/main/java/com/eazybites/accounts/repository/AccountsRepository.java
@@ -1,9 +1,12 @@
 package com.eazybites.accounts.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.eazybites.accounts.model.entity.Accounts;
 
 public interface AccountsRepository extends JpaRepository<Accounts, Long>{
+    Optional<Accounts> findByCustomerId(Long customerId);
 
 }
