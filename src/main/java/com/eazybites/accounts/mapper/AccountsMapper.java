cc6a3cb7f2d99108e017670ff96789045ebe876b
joselopez <alberto.esteva88@gmail.com>
2025-06-13 23:44:12 -0600

    Corrigiendo mappers

--- src/main/java/com/eazybites/accounts/mapper/AccountsMapper.java
+++ src/main/java/com/eazybites/accounts/mapper/AccountsMapper.java
@@ -1,19 +1,18 @@
 package com.eazybites.accounts.mapper;
 
+import org.mapstruct.InheritConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-import com.eazybites.accounts.model.dto.request.AccountsDto;
+import com.eazybites.accounts.model.dto.response.AccountResponseDto;
 import com.eazybites.accounts.model.entity.Accounts;
 
 @Mapper(componentModel = "spring")
-public interface AccountsMapper {
-    AccountsDto mapToAccountsDto(Accounts accounts);
-    @Mapping(target = "accountNumber", ignore = true)
-    @Mapping(target = "createdAt", ignore = true)
-    @Mapping(target = "createdBy", ignore = true)
-    @Mapping(target = "updatedAt", ignore = true)
-    @Mapping(target = "updatedBy", ignore = true)
-    Accounts mapToAccounts(AccountsDto accountsDto);
+public interface AccountsMapper extends AuditableMappingConfig<AccountResponseDto, Accounts> {
+    @InheritConfiguration(name = "mapAudit")
+    @Mapping(target = "customerId", ignore = true)
+    Accounts mapToAccount(AccountResponseDto dto);
+
+    AccountResponseDto mapToAccountDto(Accounts account);
 
 }
--- src/main/java/com/eazybites/accounts/mapper/CustomerMapper.java
+++ src/main/java/com/eazybites/accounts/mapper/CustomerMapper.java
@@ -1,18 +1,21 @@
 package com.eazybites.accounts.mapper;
 
+import org.mapstruct.InheritConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-import com.eazybites.accounts.model.dto.request.CustomerDto;
+import com.eazybites.accounts.model.dto.request.CustomerRequestDto;
+import com.eazybites.accounts.model.dto.response.CustomerResponseDto;
 import com.eazybites.accounts.model.entity.Customer;
 
 @Mapper(componentModel = "spring")
-public interface CustomerMapper  {
-    CustomerDto mapToCustomerDto(Customer customer);
-    @Mapping(target = "customerId", ignore = true)
-    @Mapping(target = "createdAt", ignore = true)
-    @Mapping(target = "createdBy", ignore = true)
-    @Mapping(target = "updatedAt", ignore = true)
-    @Mapping(target = "updatedBy", ignore = true)
-    Customer mapToCustomer(CustomerDto customerDto);
+public interface CustomerMapper extends AuditableMappingConfig<CustomerResponseDto, Customer> {
+    @Mapping(target = "account", ignore = true)
+    CustomerResponseDto mapToCustomerDto(Customer customer);
+    
+    @InheritConfiguration(name = "mapAudit")
+    Customer mapToCustomer(CustomerResponseDto customerDto);
+
+     @InheritConfiguration(name = "mapAudit")
+    Customer mapToCustomer(CustomerRequestDto customerDto);
 }
