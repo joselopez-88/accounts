a8c228cd60369c12c63db5248847da60d4049524
joselopez <alberto.esteva88@gmail.com>
2025-06-15 01:33:04 -0600

    Agregando metodo eliminar por customerId en AccountsRepository

--- src/main/java/com/eazybites/accounts/repository/AccountsRepository.java
+++ src/main/java/com/eazybites/accounts/repository/AccountsRepository.java
@@ -3,10 +3,17 @@ package com.eazybites.accounts.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.eazybites.accounts.model.entity.Accounts;
 
+
 public interface AccountsRepository extends JpaRepository<Accounts, Long>{
     Optional<Accounts> findByCustomerId(Long customerId);
+
+    @Transactional
+    @Modifying
+    void deleteByCustomerId(Long customerId);
 
 }
