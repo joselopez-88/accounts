819abd49f166a9c8ba458b7c665671233bf5723c
joselopez <alberto.esteva88@gmail.com>
2025-06-18 14:28:30 -0600

    Modificando la configuracion para los campos de auditoria, creando clase propia

---
+++ src/main/java/com/eazybites/accounts/model/audit/AuditAwareImpl.java
@@ -0,0 +1,16 @@
+package com.eazybites.accounts.model.audit;
+import java.util.Optional;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+@Component("auditAwareImpl")
+public class AuditAwareImpl implements AuditorAware<String>{
+
+    @Override
+    @NonNull
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of("Annonymous");
+    }
+
+}
