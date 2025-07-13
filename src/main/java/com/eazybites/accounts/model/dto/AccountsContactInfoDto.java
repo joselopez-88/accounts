b48be7be18196deb17190fe744f7a6cd6cb35e1d
joselopez <alberto.esteva88@gmail.com>
2025-07-13 17:28:26 -0600

    Actualizacion en tiempo de ejecucion de valores de properties o yml que usan @ConfigurationProperties- 1 - Convertir el record en una clase normal

--- src/main/java/com/eazybites/accounts/model/dto/AccountsContactInfoDto.java
+++ src/main/java/com/eazybites/accounts/model/dto/AccountsContactInfoDto.java
@@ -4,8 +4,16 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-@ConfigurationProperties(prefix = "accounts")// Esto sirve para tomar los datos del application.properties o yml con el prefijo accounts con los nombres de las propiedades
-public record AccountsContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
 
+import lombok.Getter;
+import lombok.Setter;
+@ConfigurationProperties(prefix = "accounts")// Esto sirve para tomar los datos del application.properties o yml con el prefijo accounts con los nombres de las propiedades
+@Getter
+@Setter
+public class AccountsContactInfoDto {
+
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
 
 }
